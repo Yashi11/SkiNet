ab61d2faa3de99c54b7ac25e2ce06bff6841d47a
Yashi11 <yashasvi488@live.com>
2023-09-19 21:38:48 +0530

    Built Initial UI with Angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {HttpClientModule} from '@angular/common/http';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { ShopModule } from './shop/shop.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CoreModule,
+    SharedModule,
+    ShopModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,11 @@
+
+  export interface IProduct {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    imageURL: string;
+    productType: string;
+    productBrand: string;
+  }
+  
