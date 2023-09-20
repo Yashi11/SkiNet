1efa0999cb5f5d936b3e850d009e899415c2ff13
Yashi11 <yashasvi488@live.com>
2023-09-20 19:12:03 +0530

    Handling&Troubleshooting errors+HTTP Interceptors

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { ShopModule } from './shop/shop.module';
 import { HomeComponent } from './home/home.component';
 import { HomeModule } from './home/home.module';
+import { ContactusComponent } from './contactus/contactus.component';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 @NgModule({
   declarations: [
     AppComponent,
+    ContactusComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,7 +26,9 @@ import { HomeModule } from './home/home.module';
     SharedModule,
     HomeModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass:ErrorInterceptor,multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
